404e1b2e4b9a62abf71e8c2c358ec2792b7aff4c
Sayan Mondal <154863207+sayanm085@users.noreply.github.com>
2025-01-25 22:17:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,45 +7,45 @@ import { PORT } from "./constants.js";
 
 dotenv.config();
 
-// let totalCPUs = os.cpus().length;
-// console.log(`Total CPUs: ${totalCPUs}`);
+let totalCPUs = os.cpus().length;
+console.log(`Total CPUs: ${totalCPUs}`);
 
-// if(cluster.isMaster){
-//     const cpus = os.cpus().length;
-//     for(let i = 0; i < cpus; i++){
-//         cluster.fork();
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`Worker ${worker.process.pid} died`);
-//         cluster.fork();
-//     });
-
-// }
-
-// if(cluster.isWorker){
-//     connectDB()
-//     .then(() => {
-//         app.listen(process.env.PORT || 8000, () => {
-//             console.log(`Server is running on port http://localhost:${process.env.PORT} ||8000`);
-//         });
-//     })
-//     .catch((error) => {
-//         console.log("error in db connection" , error);
-//         process.exit(1);
-//     });
-// }
-
-
-
-
-
-connectDB()
-.then(() => {
-    app.listen(PORT || 8000, () => {
-        console.log(`Server is running on port http://localhost:${PORT} ||8000`);
+if(cluster.isMaster){
+    const cpus = os.cpus().length;
+    for(let i = 0; i < cpus; i++){
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork();
     });
-})
-.catch((error) => {
-    console.log("error in db connection" , error);
-    process.exit(1);
-});
+
+}
+
+if(cluster.isWorker){
+    connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port http://localhost:${process.env.PORT} ||8000`);
+        });
+    })
+    .catch((error) => {
+        console.log("error in db connection" , error);
+        process.exit(1);
+    });
+}
+
+
+
+
+
+// connectDB()
+// .then(() => {
+//     app.listen(PORT || 8000, () => {
+//         console.log(`Server is running on port http://localhost:${PORT} ||8000`);
+//     });
+// })
+// .catch((error) => {
+//     console.log("error in db connection" , error);
+//     process.exit(1);
+// });
